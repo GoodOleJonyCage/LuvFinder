619cf87e67b839d837b428f60e77eab42a5b7743
GoodOleJonyCage <adil_ahsan_khan@yahoo.com>
2023-08-26 11:19:13 -0400

    Fixed  bug in profile questions page

--- ClientApp/src/components/ProfileQuestionnaire.js
+++ ClientApp/src/components/ProfileQuestionnaire.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2b94495be3413820981f17bd6c800aaa6a1df968510a3580246dbd1ab385389a
-size 17772
+oid sha256:0f0ada756c60359866115a90a4c9d97a901db9774278b227776fecdbd5aa480d
+size 17992
